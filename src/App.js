c1e593c627805287ea13698b05e60b3a23eb686e
Luis Leno <luizleno15@gmail.com>
2023-03-30 14:06:02 -0300

    configure kompassify 20

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import { useEffect } from 'react';
 
 function App() {
 
+//bf27e869-faa2-4427-ae52-c25a73fea247
 
   useEffect(()=> {
 
     window.addEventListener('message', function (event) {
       if (event.data.TYPE === "KOMPASSIFY_BOOT_LOADER_IS_READY") {
-        window?.kompassifyLaunchTour('04962af1-e082-4f23-bb8c-e0b603e58a77', 0, 1);
+        // window?.kompassifyLaunchTour('04962af1-e082-4f23-bb8c-e0b603e58a77', 0, 1);
+        window?.kompassifyProgressBar.startProgressPlayer('bf27e869-faa2-4427-ae52-c25a73fea247');
       }
   });
         
-  }, [window.kompassifyLaunchTour])
+  }, [window.kompassifyProgressBar])
   return (
     <div className="App">
       <header className="App-header">
